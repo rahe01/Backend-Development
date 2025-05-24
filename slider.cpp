060f4f8f1dfe8920ba63e4c285285267e9aaf913
Rahe <130391604+rahe01@users.noreply.github.com>
2025-05-24 23:38:41 +0600

    dfs

--- slider.cpp
+++ slider.cpp
@@ -1,1 +1,16 @@
-//sdfjshdfjhsdf
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+
+    cout<< "Hello";
+    
+
+    return 0;
+}
+
